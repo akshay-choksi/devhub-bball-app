d7b00251c1d0271ba96f6bbdf7e0505e5fa9104a
Caitlyn Fay <cpf8vj@virginia.edu>
2022-02-22 16:02:26 -0500

    practice

--- src/screens/Stats.js
+++ src/screens/Stats.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { ActivityIndicator, FlatList, View } from 'react-native';
 import { Layout, Text } from 'react-native-rapi-ui';
 
 export default function ({ navigation }) {
+	const [isLoading, setLoading] = useState(true);
+  	const [data, setData] = useState([]);
+  	const getGames = async () => {
+     	try {
+     		const response = await fetch('http://35.153.97.187:8080/teams');
+      		const json = await response.json();
+      		setData(json.data);
+    	} catch (error) {
+      		console.error(error);
+    	} finally {
+      		setLoading(false);
+    	}
+  	}
+	  
+	useEffect(() => {
+		getGames();
+	}, []);
+	
 	return (
 		<Layout>
 			<View
@@ -12,7 +30,15 @@ export default function ({ navigation }) {
 					justifyContent: 'center',
 				}}
 			>
-				<Text>This is the Stats tab</Text>
+				{isLoading ? <ActivityIndicator/> : (
+        		<FlatList
+          			data={data}
+          			keyExtractor={({ id }, index) => id}
+          			renderItem={({ item }) => (
+            		<Text>{item.team_name}</Text>
+          			)}
+        		/>
+      			)}
 			</View>
 		</Layout>
 	);
