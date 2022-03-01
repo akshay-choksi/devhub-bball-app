c775e9f5ff09f12ed050770f3179c4a71e59df0d
Caitlyn Fay <cpf8vj@virginia.edu>
2022-03-01 10:13:58 -0500

    background color for statistics page

--- src/screens/Stats.js
+++ src/screens/Stats.js
@@ -1,45 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import { ActivityIndicator, FlatList, View } from 'react-native';
-import { Layout, Text } from 'react-native-rapi-ui';
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-export default function ({ navigation }) {
-	const [isLoading, setLoading] = useState(true);
-  	const [data, setData] = useState([]);
-  	const getGames = async () => {
-     	try {
-     		const response = await fetch('http://35.153.97.187:8080/teams');
-      		const json = await response.json();
-      		setData(json.data);
-    	} catch (error) {
-      		console.error(error);
-    	} finally {
-      		setLoading(false);
-    	}
-  	}
-	  
-	useEffect(() => {
-		getGames();
-	}, []);
-	
-	return (
-		<Layout>
-			<View
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				{isLoading ? <ActivityIndicator/> : (
-        		<FlatList
-          			data={data}
-          			keyExtractor={({ id }, index) => id}
-          			renderItem={({ item }) => (
-            		<Text>{item.team_name}</Text>
-          			)}
-        		/>
-      			)}
-			</View>
-		</Layout>
-	);
-}
+const image = { uri: "https://playrockwall.com/wp-content/uploads/2020/04/basketballs.png" };
+
+const App = () => (
+  <View style={styles.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Game Statistics</Text>
+    </ImageBackground>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+});
+
+export default App;
