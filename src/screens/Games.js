8228bf99747914feb8dac64b1c4e5b28ec7d675c
nour <ng9sc@virginia.edu>
2022-03-14 22:34:33 -0400

    added react native API

--- src/screens/Games.js
+++ src/screens/Games.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { Layout, Text } from 'react-native-rapi-ui';
+import { ThemeProvider } from "react-native-rapi-ui";
+import { ActivityIndicator, FlatList } from 'react-native';
 
-export default function ({ navigation }) {
+export default App = () => {
+	const [isLoading, setLoading] = useState(true);
+	const [data, setData] = useState([]);
+  
+	const getMovies = async () => {
+	   try {
+		const response = await fetch('http://35.153.97.187:8080/schedule/games');
+		const json = await response.json();
+		setData(json.movies);
+	  } catch (error) {
+		console.error(error);
+	  } finally {
+		setLoading(false);
+	  }
+	}
+  
+	useEffect(() => {
+	  getMovies();
+	}, []);
+  
 	return (
-		<Layout>
-			<View
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				<Text>This is the Games tab</Text>
-			</View>
-		</Layout>
+	  <View style={{ flex: 1, padding: 24 }}>
+		{isLoading ? <ActivityIndicator/> : (
+		  <FlatList
+			data={data}
+			keyExtractor={({ id }, index) => id}
+			renderItem={({ item }) => (
+			  <Text>{item.title}, {item.releaseYear}</Text>
+			)}
+		  />
+		)}
+	  </View>
 	);
-}
+  };
