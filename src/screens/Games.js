4f0835a92a35e4b7893c067dc4f18710dc4f8614
nour <ng9sc@virginia.edu>
2022-04-01 19:16:37 -0400

    TO-DO: display FlatList

--- src/screens/Games.js
+++ src/screens/Games.js
@@ -1,73 +1,63 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
 import { Layout, Text } from 'react-native-rapi-ui';
-import { ThemeProvider } from "react-native-rapi-ui";
-import { ActivityIndicator, FlatList } from 'react-native';
 
-export default App = () => {
+export default function ({ navigation }) {
+	const [name, setName] = useState([]);
 	const [isLoading, setLoading] = useState(true);
-	const [data, setData] = useState([]);
-	// nested text
-	const ReactText = () => {
-		const [titleText, setTitleText] = useState("All Games");
-		const bodyText = "A complete list of all games stats.";
-	  
-		const onPressTitle = () => {
-		  setTitleText("Basketball Games");
-		};
-	  
-		return (
-		  <Text style={styles.baseText}>
-			<Text style={styles.titleText} onPress={onPressTitle}>
-			  {titleText}
-			  {"\n"}
-			  {"\n"}
-			</Text>
-			<Text numberOfLines={5}>{bodyText}</Text>
-		  </Text>
-		);
-	  };
-	  
-	  const styles = StyleSheet.create({
-		baseText: {
-		  fontFamily: "Cochin"
-		},
-		titleText: {
-		  fontSize: 20,
-		  fontWeight: "bold"
-		}
-	  });
-	  
-	  export default ReactText;
-
-	// gets games by year
-	const getGames = async () => {
-	   try {
-		const response = await fetch('http://35.153.97.187:8080/schedule/games');
-		const json = await response.json();
-		setData(json.movies);
-	  } catch (error) {
-		console.error(error);
-	  } finally {
-		setLoading(false);
-	  }
-	}
-  
 	useEffect(() => {
-	  getGames();
-	}, []);
-  
+		names();
+	}, [])
+
+	const names = async () => {
+		try {
+			const response = await fetch('http://35.153.97.187:8080/schedule/games');
+			const json = await response.json();
+			setName(json.data);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	}
+
 	return (
-	  <View style={{ flex: 1, padding: 24 }}>
-		{isLoading ? <ActivityIndicator/> : (
-		  <FlatList
-			data={data}
-			keyExtractor={({ id }, index) => id}
-			renderItem={({ item }) => (
-			  <Text>{item.title}, {item.releaseYear}</Text>
-			)}
-		  />
-		)}
-	  </View>
+		<View>
+			<Text>Games</Text>
+			{isLoading ? <ActivityIndicator /> : (
+				<SafeAreaView>
+				<FlatList>
+					data={name}
+					keyExtractor={({ id }, index) => id}
+					renderItem={({ item }) => (
+						<Text>{item.pick}</Text>
+					)}
+				</FlatList>
+				</SafeAreaView>
+			)
+			}
+		</View>
 	);
-  };
+}
+/*
+		return (
+		<Layout>
+			<View
+				style={{
+					flex: 1,
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				<Text>Games:</Text>
+				<FlatList>
+					data={name}
+					keyExtractor={({ id }, index) => id}
+					renderItem={({ item }) => (
+					<Text>{item.pick}</Text>
+						)}
+				</FlatList>
+			</View>
+		</Layout>
+	);
+	*/
--- src/screens/Teams.js
+++ src/screens/Teams.js
@@ -1,19 +1,93 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Layout, Text } from 'react-native-rapi-ui';
+import { View,FlatList, ActivityIndicator } from 'react-native';
+import { Layout, Text, Section, SectionContent, Picker } from 'react-native-rapi-ui';
 
 export default function ({ navigation }) {
+    /*
+	const [isLoading, setLoading] = useState(true);
+  	const [data, setData] = useState([]);
+  	const getGames = async () => {
+     	try {
+     		const response = await fetch('http://35.153.97.187:8080/teams');
+      		const json = await response.json();
+      		setData(json.data);
+    	} catch (error) {
+      		console.error(error);
+    	} finally {
+      		setLoading(false);
+    	}
+  	}
+	useEffect(() => {
+		getGames();
+	}, []);
+    */
+    const [pickerValue, setPickerValue] = React.useState(null);
+    const items = [
+        { label: 'American', value: 'Amer' },
+        { label: 'Atlantic Coastal Conference', value: 'ACC' },
+        { label: 'Atlantic 10', value: 'A10' },
+    ];
+
 	return (
 		<Layout>
 			<View
 				style={{
 					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
+					alignItems: 'left',
+					justifyContent: 'left',
 				}}
 			>
-				<Text>This is the Profile tab</Text>
+				<Text>NCAA D1 Basketball Teams</Text>
 			</View>
+            <Section style={{ marginHorizontal: 20, marginTop: 20 }}>
+            <SectionContent>
+                <View>
+                    <Text style={{ marginBottom: 10 }}>Picker</Text>
+                    <Picker
+                        items={items}
+                        value={pickerValue}
+                        placeholder="Choose your Conference"
+                        onValueChange={(val) => setPickerValue(val)}
+                    />
+                </View>
+            </SectionContent>
+        </Section>
 		</Layout>
-	);
+    );
 }
+
+/*
+//Add a teams 'Section'
+<Section>
+    <SectionContent>
+        <Text> NCAA DI Teams </Text>
+    </SectionContent>
+</Section>
+;
+//
+//Conferences Picker
+const Forms = () => {
+    const [pickerValue, setPickerValue] = React.useState(null);
+    const items = [
+        { label: 'American', value: 'Amer' },
+        { label: 'Atlantic Coastal Conference', value: 'ACC' },
+        { label: 'Atlantic 10', value: 'A10' },
+    ];
+    return (
+        <Section style={{ marginHorizontal: 20, marginTop: 20 }}>
+            <SectionContent>
+                <View>
+                    <Text style={{ marginBottom: 10 }}>Picker</Text>
+                    <Picker
+                        items={items}
+                        value={pickerValue}
+                        placeholder="Choose your Conference"
+                        onValueChange={(val) => setPickerValue(val)}
+                    />
+                </View>
+            </SectionContent>
+        </Section>
+    );
+};
+export default Forms;
+*/
