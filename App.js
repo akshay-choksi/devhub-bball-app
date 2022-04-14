b80757cb0bb0466945ed8af4463ed72e2c007a81
glendonC <57140230+glendonC@users.noreply.github.com>
2022-04-14 11:49:26 -0400

    Revert "adding home page"

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { ThemeProvider } from "react-native-rapi-ui";
-import "./index.css"
 export default function App() {
   return (
     <ThemeProvider>
