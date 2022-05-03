df3b9230b28c8d1ca48750498fe1e14fc9e9fe07
nour <ng9sc@virginia.edu>
2022-05-03 15:04:40 -0400

    Start of table

--- src/screens/Games.js
+++ src/screens/Games.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { View, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
-import { Layout, Text } from 'react-native-rapi-ui';
+import { Text } from 'react-native-rapi-ui';
+import { useTable } from 'react-table'
 
 export default function ({ navigation }) {
 	const [name, setName] = useState([]);
@@ -21,54 +22,152 @@ export default function ({ navigation }) {
 		}
 	}
 
+	const data = React.useMemo(
+		() => [
+			{
+				home: 'Home Team',
+				home_score: 'Home Score',
+			},
+			{
+				away: 'Away Team',
+				away_score: 'Away Score',
+			},
+			{
+				spread: 'Spread',
+				value: 'Value',
+			},
+			{
+				pick: 'Pick',
+				level: 'Level',
+				final_result: 'Final Result'
+			}
+		]
+	)
+
+	const columns = React.useMemo(
+		() => [
+			{
+				Header: 'Home Team',
+				accessor: 'home'
+			},
+			{
+				Header: 'Home Score',
+				accessor: 'home_score'
+			},
+			{
+				Header: 'Away Team',
+				accessor: 'away'
+			},
+			{
+				Header: 'Away Score',
+				accessor: 'away_score'
+			},
+			{
+				Header: 'Spread',
+				accessor: 'spread'
+			},
+			{
+				Header: 'value',
+				accessor: 'value'
+			},
+			{
+				Header: 'Pick',
+				accessor: 'pick'
+			},
+			{
+				Header: 'Level',
+				accessor: 'level'
+			},
+			{
+				Header: 'Final Result',
+				accessor: 'final_result'
+			}
+		]
+	)
+
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		rows,
+		prepareRow,
+	} = useTable({ columns, data })
+
 	return (
 		<View
-		style={{
-			flex: 1,
-			alignItems: 'left',
-			justifyContent: 'left',
-			marginTop: 20,
-			marginLeft: 5
-		}}>
+			style={{
+				flex: 1,
+				alignItems: 'left',
+				justifyContent: 'left',
+				marginTop: 20,
+				marginLeft: 5
+			}}>
 			<Text>Games</Text>
 			{isLoading ? <ActivityIndicator /> : (
-				<SafeAreaView>
-				<FlatList
-					data={name}
-					keyExtractor={( item, index) => item.key}
-					renderItem={({ item }) => (
-						<Text>
-							{item.home}
-							{item.away}
-``							{item.pick}
-							</Text>
-					)}
-				/>
-				</SafeAreaView>
+				<Text>
+					<table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+						<thead>
+							{headerGroups.map(headerGroup => (
+								<tr {...headerGroup.getHeaderGroupProps()}>
+									{headerGroup.headers.map(column => (
+										<th
+											{...column.getHeaderProps()}
+											style={{
+												borderBottom: 'solid 3px red',
+												background: 'aliceblue',
+												color: 'black',
+												fontWeight: 'bold',
+											}}
+										>
+											{column.render('Header')}
+										</th>
+									))}
+								</tr>
+							))}
+						</thead>
+						<tbody {...getTableBodyProps()}>
+							{rows.map(row => {
+								prepareRow(row)
+								return (
+									<tr {...row.getRowProps()}>
+										{row.cells.map(cell => {
+											return (
+												<td
+													{...cell.getCellProps()}
+													style={{
+														padding: '10px',
+														border: 'solid 1px gray',
+													}}
+												>
+													{cell.render('Cell')}
+												</td>
+											)
+										})}
+									</tr>
+								)
+							})}
+						</tbody>
+					</table>
+				</Text>
 			)
 			}
 		</View>
 	);
 }
+
+
 /*
-		return (
-		<Layout>
-			<View
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				<Text>Games:</Text>
-				<FlatList>
-					data={name}
-					keyExtractor={({ id }, index) => id}
-					renderItem={({ item }) => (
-					<Text>{item.pick}</Text>
-						)}
-				</FlatList>
-			</View>
-		</Layout>
-	);
+{
+  Data.filter(post => {
+	if (query === '') {
+	  return post;
+	} else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+	  return post;
+	}
+  }).map((post, index) => (
+	<div className="box" key={index}>
+	  {post.title}
+	  {post.author}
+	  ))
+}	);
 	*/
